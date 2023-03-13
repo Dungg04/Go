219ea051c17890d239debc491fc0b3a6ab1ef4b1
Dungg04 <lydungyd@gmail.com>
2023-03-13 17:40:31 +0700

    push api login logout register forgotPass

---
+++ Auth_GO/services/auth.service.go
@@ -0,0 +1,7 @@
+package services
+
+
+type AuthService interface {
+	SignUpUser(*models.SignUpInput) (*models.DBResponse, error)
+	SignInUser(*models.SignInInput) (*models.DBResponse, error)
+}
---
+++ Auth_GO/services/user.service.go
@@ -0,0 +1,9 @@
+package services
+
+
+type UserService interface {
+	FindUserById(id string) (*models.DBResponse, error)
+	FindUserByEmail(email string) (*models.DBResponse, error)
+	UpdateUserById(id string, field string, value string) (*models.DBResponse, error)
+	UpdateOne(field string, value interface{}) (*models.DBResponse, error)
+}
